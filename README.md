240290cb939dee1601e1149b1ca757ca74df95a6
Elijah <lihaoxuan@vsoul.cn>
2024-06-06 11:52:21 +0800

    init

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # kube-go
 
+|                   | Kubernetes1.24 | Kubernetes1.25 | Kubernetes1.26 | Kubernetes1.27 | Kubernetes1.28 | Kubernetes1.29 | Kubernetes1.30 |
+|:-----------------:|:--------------:|:--------------:|:--------------:|:--------------:|:--------------:|:--------------:|:--------------:|
+| `kube-go版本v0.1.0` |       ✕        |       ✓        |       ✓        |       ✓        |       ✓        |       ✓        |       ✓        |
+
+
 打造通用kubernetes api
 
 - [x] 基于gin框架的后端api
 - [ ] 集成client-go
 - [ ] 集成GinSwagger
 - [ ] 集成mysql、redis
+
+## 快速开始
+
+```shell
+go mod tidy
+go run
+```
--- main/mian.go
+++ main/mian.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/sreok/kube-go/routers/api"
+	_ "github.com/sreok/kube-go/docs"
+	"github.com/sreok/kube-go/routers"
+	swaggerFiles "github.com/swaggo/files"
+	gs "github.com/swaggo/gin-swagger"
 	"log"
 )
 
+// @title kube-go
+// @version 0.1.0
+// @description 基于client-go和gin框架后端api服务
+// @termsOfService https://github.com/sreok/kube-go
 func main() {
-	router := gin.Default()
+	route := gin.Default()
 
-	router.GET("/", api.Index)
+	route.GET("/swagger/*any", gs.WrapHandler(swaggerFiles.Handler))
+	api := route.Group("/api")
+	{
+		api.GET("/", routers.Index)
+	}
+
 	// 指定地址和端口号
-	err := router.Run(":8080")
+	err := route.Run(":8080")
 	if err != nil {
 		log.Println(err)
 		return
